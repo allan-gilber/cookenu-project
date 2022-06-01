7ccc6ed7fccb0428cfce49012866318f28fc599c
allan-gilber <allangilber@yahoo.com.br>
2022-06-01 00:12:04 -0300

    fix: changed the fields that are returned by the feed enpoint.

--- src/data/FeedData.ts
+++ src/data/FeedData.ts
@@ -3,10 +3,12 @@ import DataBase from '../services/DataBase';
 export default class FeedData extends DataBase {
 	async requestUserFeed (userId: string) {
 		return await this.connection()
-			.select('followers.followed_id', 'recipes.recipe_id', 'recipes.recipe_title', 'recipes.recipe_description','recipes.recipe_creator_id', 'recipes.recipe_creation_date')
+			.select('users.user_name as creator_name', 'followers.followed_id as creator_id', 'recipes.recipe_id', 'recipes.recipe_title', 'recipes.recipe_description', 'recipes.recipe_creation_date')
 			.from('followers')
 			.where('followers.follower_id', '=', userId)
+			// .orWhere('creator_name', '=', userId)
 			.innerJoin('recipes', 'recipes.recipe_creator_id', 'followers.followed_id').orderBy('recipes.recipe_creation_date', 'desc')
+			.innerJoin('users', 'users.user_id', 'followers.followed_id')
 			.then((response): any => {
 				if(!response) return []; 
 				return response;
